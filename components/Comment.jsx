4a11b80d18e899697296da889e6d0054479807fa
charnchol <khumball46@gmail.com>
2022-09-15 14:54:16 +0700

    sad

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Reply from "./Reply";
+import { comments } from "../libs/comments";
 
 export default function Comment(props) {
+  const c = comments;
+  const d = comments.filter((x) => x.replies);
   return (
     <div>
       {/* Entire Comment div */}
-      <div></div>
+      {c.map((x, i) => (
+        <div className="d-flex gap-2 my-2" key={i}>
+          <img
+            src={x.userImagePath}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+              {x.username}
+            </span>
+            <br />
+            <span style={{ color: "#E4E6EB" }}>{x.commentText}</span>
+            <div className="d-flex align-items-center gap-1">
+              {/* if({x.likeNum})  */}
+              <img src="/like.svg" width={20}></img>
+              <span style={{ color: "#B0B3B8" }}>{x.likeNum} คน</span>
+            </div>
+          </div>
+          {/* if({x.replies}) <Reply /> */}
+        </div>
+      ))}
 
       {/*render Reply here... */}
+      <Reply />
     </div>
   );
 }
